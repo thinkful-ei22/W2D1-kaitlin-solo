8a69ce76a4b27b9ded66e3dc66630d92b20d10aa
Kaitlin Bunn <kabunn1991@gmail.com>
2018-06-18 12:30:23 -0400

    Update shoppingList to use Item.create working

--- scripts/Item.js
+++ scripts/Item.js
@@ -2,7 +2,21 @@
 
 const Item = (function () {
 
-  return {
+    const validateName = function(name) {
+        if (!name) {
+            throw new TypeError("Name does not exist.");
+        }
+    }
+
+    const create = function(name) {
+        return { 
+            id: cuid(),
+            name,
+            checked: false,
+        }
+    };
     
-  };
+    return {
+        validateName, create
+    }
 }() );
