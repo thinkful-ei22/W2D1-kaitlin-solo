89c1bf6d549d780a31bd57fd09b7bf65841a91c8
Kaitlin Bunn <kabunn1991@gmail.com>
2018-06-18 12:00:33 -0400

    Module exercises complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,34 @@
 /* global shoppingList, cuid */
 
 // eslint-disable-next-line no-unused-vars
-const store = {
-  items: [
+
+const store = (function() {
+  
+  const items = [
     { id: cuid(), name: 'apples', checked: false },
     { id: cuid(), name: 'oranges', checked: false },
     { id: cuid(), name: 'milk', checked: true },
     { id: cuid(), name: 'bread', checked: false }
-  ],
-  hideCheckedItems: false,
-  searchTerm: ''
-};
+  ];
+  let hideCheckedItems = false;
+  let searchTerm = '';
+
+  return {
+    items, hideCheckedItems, searchTerm
+  };
+
+}());
+
+// const store = {
+//   items: [
+//     { id: cuid(), name: 'apples', checked: false },
+//     { id: cuid(), name: 'oranges', checked: false },
+//     { id: cuid(), name: 'milk', checked: true },
+//     { id: cuid(), name: 'bread', checked: false }
+//   ],
+//   hideCheckedItems: false,
+//   searchTerm: ''
+// };
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
